c99010bb874c7b67f1599f635766b4e618b7c761
JNJS <83789293+jnjsig@users.noreply.github.com>
2023-02-23 16:44:25 +0100

    Delete cookies.js

--- cookies.js
+++
@@ -1,8 +0,0 @@
-function myFunction3() {
-  var x = document.getElementById("myDIV");
-  if (x.style.display === "none") {
-	x.style.display = "block";
-  } else {
-	x.style.display = "none";
-  }
-}
