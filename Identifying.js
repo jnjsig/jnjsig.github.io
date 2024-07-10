618293be8f9e973a6871f6c67649701094187cb3
JNJS <83789293+jnjsig@users.noreply.github.com>
2024-07-10 17:48:11 +0200

    Add files via upload

---
+++ Identifying.js
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+    <html lang='en'>
+      <meta name='viewport' content='width=device-width, initial-scale=1'>
+      <meta charset='UTF-8'>
+      <head>
+          <title>Nathan's Fullness</title>
+      <link rel='icon' type='image/x-icon' href='jnjs.ico'>
+      <link rel='stylesheet' href='styles.css'>
+      </head>
+      <body>
+      <div class='fixtheposition'>
+      <div class='topnav' id='myTopnav'>
+          <a href='index.html' class='active'><img src='logo.png' class='navbar_logo'></a>
+          <a class='navbarText' href='https://www.google.com/'>Find</a>
+          <div class='dropdown'>
+            <button class='dropbtn navbarText'>¿Where is Nathan?
+              <i class='fa fa-caret-down'></i>
+            </button>
+            <div class='dropdown-content'>
+                      <a href='https://www.google.com/maps' class='navbarText'>Google Maps</a>
+                      <a href='https://www.whatismyip.com/' class='navbarText'>What Is My IP</a>
+            </div>
+          </div>
+          <a class='navbarText' href="policy.html">Privacy</a>
+          <a href='javascript:void(0);' style='font-size: 25px;' class='icon' onclick='myFunction()'>&#9776;</a>
+      </div>
+      </div>
+      <video type='video/mp4' class='videobackground' autoPlay muted loop preLoad playsinline>
+        <source src='background.mp4'>
+      </video>
+      <center>
+      <br><br><br>
+      <h1>¿Have you tryed stealing my fullness?</h1>
+      <div>
+      <iframe style="opacity: 90%; min-height: 50%; width: 100%;" 
+        frameborder="0" scrolling="no" 
+        marginheight="0" 
+        marginwidth="0" 
+        src="https://maps.google.com/maps?f=q&amp;saddr=37.42216,-122.083737&amp;source=s_d&amp;hl=en&amp;z=15&amp;output=embed">
+      </iframe>
+      </center>
+      <script>
+        while people {
+          name_2: updateName;
+        }
+        // Sample data array
+        const people = [
+            { name_1: "Nathan"},
+            { name_2: ""}
+        ];
+        // Function to find 'Nathan'
+        function findNathan(people) {
+            return people.find(person => person.name === "Nathan");
+        }
+        // Call the function
+        const nathan = findNathan(people);
+        // Output the result
+        while (nathan) {
+            console.log("Nathan found", nathan);
+        } else {
+            console.log("Nathan not found yet.");
+        }
+      </script>
+      <p>This letter serves as a confirmation that all written materials provided by you will be returned to you in their entirety. We acknowledge and respect the ownership of the written content you have shared with us, and we are committed to ensuring its safe and prompt return.</p>
+      <p>Please be assured that:</p>
+      <p>Ownership: All written materials, SD cards, ashes, and other objects remain the sole property of Jonathan Nathan Jonas Sigmund.</p>
+      <p>Usage: The provided materials will only be used for the agreed-upon purposes and will not be reproduced, distributed, or disclosed without your explicit consent.</p>
+      <p>Return Procedure: Upon completion of the agreed-upon work or at your request, all written materials will be promptly returned to you in their original condition.</p>
+      <p>Confidentiality: Any information contained within the written materials will be kept confidential and will not be shared with any third parties without your prior approval.</p>
+      <p>We value your trust and are dedicated to maintaining the integrity and confidentiality of your written content. Should you have any specific instructions regarding the return of your materials, please do not hesitate to inform us.</p>
+      <p>Thank you for entrusting us with your written content.</p>
+      <p>Sincerely,</p>
+      <p>Jonathan Nathan Jonas Sigmund</p>
+      <p>Owner</p>
+      </p>
+      <script id="cid0020000381613919163" data-cfasync="false" async src="//st.chatango.com/js/gz/emb.js" style="width: 100%;height: 100%;">{"handle":"whereisnathan","arch":"js","styles":{"a":"ffffcc","b":100,"c":"000000","d":"000000","k":"ffffcc","l":"ffffcc","m":"ffffcc","p":"10","q":"ffffcc","r":100,"ab":false,"usricon":0,"surl":0,"cnrs":"0.35","fwtickm":1}}</script>
+    </div>
+    </body>
+</html>
